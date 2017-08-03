4ae445be340731e1f56cfd958685429c608638de
KimJeongChul <kjc5443@gmail.com>
2017-08-03 19:03:11 +0900

    Update collaborative_filtering.py

--- collaborative_filtering.py
+++ collaborative_filtering.py
@@ -62,40 +62,6 @@ def pearson_correlation(person1,person2):
 		r = numerator_value/denominator_value
 		return r
 
-def pearson_correlation(person1,person2):
- 
-	# To get both rated items
-	both_rated = {}
-	for item in dataset[person1]:
-		if item in dataset[person2]:
-			both_rated[item] = 1
- 
-	number_of_ratings = len(both_rated)		
-	
-	# Checking for number of ratings in common
-	if number_of_ratings == 0:
-		return 0
- 
-	# Add up all the preferences of each user
-	person1_preferences_sum = sum([dataset[person1][item] for item in both_rated])
-	person2_preferences_sum = sum([dataset[person2][item] for item in both_rated])
- 
-	# Sum up the squares of preferences of each user
-	person1_square_preferences_sum = sum([pow(dataset[person1][item],2) for item in both_rated])
-	person2_square_preferences_sum = sum([pow(dataset[person2][item],2) for item in both_rated])
- 
-	# Sum up the product value of both preferences for each item
-	product_sum_of_both_users = sum([dataset[person1][item] * dataset[person2][item] for item in both_rated])
- 
-	# Calculate the pearson score
-	numerator_value = product_sum_of_both_users - (person1_preferences_sum*person2_preferences_sum/number_of_ratings)
-	denominator_value = sqrt((person1_square_preferences_sum - pow(person1_preferences_sum,2)/number_of_ratings) * (person2_square_preferences_sum -pow(person2_preferences_sum,2)/number_of_ratings))
-	if denominator_value == 0:
-		return 0
-	else:
-		r = numerator_value/denominator_value
-		return r
-
 def most_similar_users(person,number_of_users):
 	# returns the number_of_users (similar persons) for a given specific person.
 	scores = [(pearson_correlation(person,other_person),other_person) for other_person in dataset if  other_person != person ]
